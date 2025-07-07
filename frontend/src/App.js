513695f481ba476f74036e10afc5625978c9e8ad
DhamariTH <dhamaritricehanson@gmail.com>
2025-07-07 15:20:07 -0400

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StrudelEditorComponent from './componets/StrudelEditorComponent'; // Assuming you saved it as StrudelEditorComponent.jsx
+
+function App() {
+    return (
+        <div className="App">
+            <StrudelEditorComponent />
+        </div>
+    );
+}
+
+export default App;
