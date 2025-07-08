eb05ca66ee5cda36b163b43379180a5426934ab3
DhamariTH <dhamaritricehanson@gmail.com>
2025-07-08 04:12:33 -0400

    Added/Learning ThemeUIProvider for theme integration Throughout Entire app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
+// src/App.js
+
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './routes/Home';
 import Invite from './routes/Invite';
+import ThemeUIProvider from './theme-ui';
+import theme from './theme';
 
 
 function App() {
     return (
-        <div className='container'>
+        <ThemeUIProvider theme={theme}>
+        <div>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    {/* If i was to scale i would create an invite page just like how thomas did */}
                     <Route path="/" element={<Invite/>}/>
                 </Routes>
             </Router>
         </div>
+        </ThemeUIProvider>
     );
 }
 
