ad84a6a5bc1cc7cf531044d1a376655422d9e43a
DhamariTH <dhamaritricehanson@gmail.com>
2025-07-08 04:13:40 -0400

    Initialized ChallengeCard component that will be used for selecting and unlocking challanges

---
+++ frontend/src/componets/ChallengeCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+// This component is a placeholder for the ChallengeCard.
+// // It will be used to display information about a challenge in the future.
+const ChallengeCard = () => {
+  return (
+    <div>ChallengeCard</div>
+  )
+}
+
+export default ChallengeCard
